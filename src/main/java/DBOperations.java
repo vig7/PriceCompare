80cf4cd0a5d3283a4dd34c99182b6e9d2408738c
isha <isha.she@media.net>
2019-08-14 12:20:16 +0530

    db operations

---
+++ src/main/java/DBOperations.java
@@ -0,0 +1,123 @@
+
+
+import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.PreparedStatement;
+        import java.sql.ResultSet;
+        import java.sql.SQLException;
+
+/**
+ * @author Crunchify.com
+ * Simple Hello World MySQL Tutorial on how to make JDBC connection, Add and Retrieve Data by App Shah
+ *
+ */
+
+public class DBOperations {
+
+
+    static Connection crunchifyConn = null;
+    static PreparedStatement crunchifyPrepareStat = null;
+
+    public static void main(String[] argv) {
+
+        try {
+            log("-------- Simple Crunchify Tutorial on how to make JDBC connection to MySQL DB locally on macOS ------------");
+            makeJDBCConnection();
+
+//            log("\n---------- Adding company 'Crunchify LLC' to DB ----------");
+            addDataToDB("Crunchify");
+//            addDataToDB("Google Inc.", "Mountain View, CA, US", 50000, "https://google.com");
+//            addDataToDB("Apple Inc.", "Cupertino, CA, US", 30000, "http://apple.com");
+
+            log("\n---------- Let's get Data from DB ----------");
+            getDataFromDB();
+
+            crunchifyPrepareStat.close();
+            crunchifyConn.close(); // connection close
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private static void makeJDBCConnection() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            log("Congrats - Seems your MySQL JDBC Driver Registered!");
+        } catch (ClassNotFoundException e) {
+            log("Sorry, couldn't found JDBC driver. Make sure you have added JDBC Maven Dependency Correctly");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            // DriverManager: The basic service for managing a set of JDBC drivers.
+            crunchifyConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test1", "root", "");
+            if (crunchifyConn != null) {
+                log("Connection Successful! Enjoy. Now it's time to push data");
+            } else {
+                log("Failed to make connection!");
+            }
+        } catch (SQLException e) {
+            log("MySQL Connection Failed!");
+            e.printStackTrace();
+            return;
+        }
+
+    }
+
+    private static void addDataToDB(String name) {
+
+        try {
+            String insertQueryStatement = "INSERT  INTO  helloworld  VALUES  (?)";
+
+            crunchifyPrepareStat = crunchifyConn.prepareStatement(insertQueryStatement);
+            crunchifyPrepareStat.setString(1, name);
+
+
+            // execute insert SQL statement
+            crunchifyPrepareStat.executeUpdate();
+            log(name + " added successfully");
+        } catch (
+
+                SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void getDataFromDB() {
+
+        try {
+            // MySQL Select Query Tutorial
+            String getQueryStatement = "SELECT * FROM helloworld";
+
+            crunchifyPrepareStat = crunchifyConn.prepareStatement(getQueryStatement);
+
+            // Execute the Query, and get a java ResultSet
+            ResultSet rs = crunchifyPrepareStat.executeQuery();
+
+            // Let's iterate through the java ResultSet
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+
+                // Simply Print the results
+                System.out.format("%s, %s, %s, %s\n", name);
+            }
+
+        } catch (
+
+                SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Simple log utility
+    private static void log(String string) {
+        System.out.println(string);
+
+    }
+}
